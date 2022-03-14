81b00e01fdef7e568a343bf0da795d8eb44109fc
Evelyn Tan <evetan.wy@gmail.com>
2022-03-14 23:25:48 +1300

    updated unwanted state

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -18,40 +18,8 @@ function Animal() {
     setTigerType(updatedSelectAnimals.tiger);
   };
 
-  //   const [isCheck, setIsCheck] = useState([]);
-  //   const [isTiger, setIsTiger] = useState(false);
-
-  //   const handleInputChange = (evt) => {
-  //     console.log(evt.target.value);
-  //     setIsCheck([...isCheck, evt.target.value]);
-  //   };
-
-  //   const handleTiger = (evt) => {
-  //     setIsTiger(!isTiger);
-  //   };
-
   return (
     <div>
-      {/* <form>
-        <label>Animal :</label>
-        <input value="Bear" type="checkbox" onChange={handleInputChange} />
-        Bear
-        <input value="Tiger" type="checkbox" onChange={handleTiger} />
-        Tiger
-        <input value="Snake" type="checkbox" onChange={handleInputChange} />
-        Snake
-        <input value="Donkey" type="checkbox" onChange={handleInputChange} />
-        Donkey
-        <div>
-          {isTiger ? (
-            <p>
-              Type of Tiger<input placeholder="type"></input>
-            </p>
-          ) : (
-            ""
-          )}
-        </div>
-      </form> */}
       <label>
         <input
           type="checkbox"
