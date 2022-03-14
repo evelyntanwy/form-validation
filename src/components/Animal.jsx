c7a8f5adb3043e3d9090431494b44c5a80049da2
Evelyn Tan <evetan.wy@gmail.com>
2022-03-14 23:12:52 +1300

    completed form

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 
 function Animal() {
-  const [isCheck, setIsCheck] = useState([]);
-  const [isTiger, setIsTiger] = useState(false);
+  const [selectAnimals, setSelectAnimals] = useState({
+    bear: false,
+    tiger: false,
+    snake: false,
+    donkey: false,
+  });
 
-  const handleInputChange = (evt) => {
-    console.log(evt.target.value);
-    setIsCheck([...isCheck, evt.target.value]);
+  const [tigerType, setTigerType] = useState(false);
+
+  const handleOnChange = (animal) => {
+    let updatedSelectAnimals = selectAnimals;
+    updatedSelectAnimals[animal] = !updatedSelectAnimals[animal];
+
+    setSelectAnimals(updatedSelectAnimals);
+    setTigerType(updatedSelectAnimals.tiger);
   };
 
-  const handleTiger = (evt) => {
-    setIsTiger(!isTiger);
-  };
+  //   const [isCheck, setIsCheck] = useState([]);
+  //   const [isTiger, setIsTiger] = useState(false);
+
+  //   const handleInputChange = (evt) => {
+  //     console.log(evt.target.value);
+  //     setIsCheck([...isCheck, evt.target.value]);
+  //   };
+
+  //   const handleTiger = (evt) => {
+  //     setIsTiger(!isTiger);
+  //   };
 
   return (
     <div>
-      <form>
+      {/* <form>
         <label>Animal :</label>
         <input value="Bear" type="checkbox" onChange={handleInputChange} />
         Bear
@@ -34,7 +51,53 @@ function Animal() {
             ""
           )}
         </div>
-      </form>
+      </form> */}
+      <label>
+        <input
+          type="checkbox"
+          label="bear"
+          value={selectAnimals.bear}
+          onChange={(e) => handleOnChange(e.target.getAttribute("label"))}
+        />{" "}
+        Bear
+      </label>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          label="tiger"
+          value={selectAnimals.tiger}
+          onChange={(e) => handleOnChange(e.target.getAttribute("label"))}
+        />{" "}
+        Tiger
+      </label>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          label="snake"
+          value={selectAnimals.snake}
+          onChange={(e) => handleOnChange(e.target.getAttribute("label"))}
+        />{" "}
+        Snake
+      </label>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          label="donkey"
+          value={selectAnimals.donkey}
+          onChange={(e) => handleOnChange(e.target.getAttribute("label"))}
+        />{" "}
+        Donkey
+      </label>
+      {tigerType ? (
+        <p>
+          Type of Tiger<input placeholder="type"></input>
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
